7dd01e3f5cc126e91fcac7c2f0db0a2e5e8a3715
satyamishra5309 <satyamishra5309@gmail.com>
2026-07-14 17:49:20 +0530

    fixed port

--- Backend/server.js
+++ Backend/server.js
@@ -38,7 +38,7 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    app.listen(PORT, "127.0.0.1", () => {
+    app.listen(PORT, () => {
       console.log(`Server running on http://127.0.0.1:${PORT}`);
     });
   } catch (error) {
